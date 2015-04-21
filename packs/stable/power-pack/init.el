c71f33d219f804e56e3b22f544b565c0a586f7c3
Alex Petrov <oleksandr.petrov@gmail.com>
2015-04-21 23:03:02 +0200

    Remove gh

--- packs/stable/power-pack/init.el
+++ packs/stable/power-pack/init.el
@@ -9,7 +9,6 @@
 (live-load-config-file "undo-tree-conf.el")
 (live-load-config-file "scratch-conf.el")
 (live-load-config-file "highlight-tail-conf.el")
-(live-load-config-file "gist-conf.el")
 (live-load-config-file "browse-kill-ring-conf.el")
 (live-load-config-file "expand-region-conf.el")
 (live-load-config-file "ace-jump-mode-conf.el")
