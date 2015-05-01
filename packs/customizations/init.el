f477730fef9fe74f1f5fb6b4b7dd761bb2b057de
Alex Petrov <oleksandr.petrov@gmail.com>
2015-05-01 17:55:43 +0200

    Add customization-pack

---
+++ packs/customizations/init.el
@@ -0,0 +1,75 @@
+;; User pack init file
+;;
+;; User this file to initiate the pack configuration.
+;; See README for more information.
+
+;; Load bindings config
+(live-load-config-file "bindings.el")
+(live-load-config-file "smex.el")
+(live-load-config-file "ido.el")
+
+;;
+;; Tabbar
+;;
+
+(require 'tabbar)
+
+(custom-set-variables
+ '(tabbar-background-color "black")
+ '(tabbar-mode t nil (tabbar))
+ '(tabbar-scroll-left-button (quote ((" <") " =")))
+ '(tabbar-scroll-right-button (quote ((" >") " =")))
+ '(tabbar-separator (quote ("  |  ")))
+ '(tabbar-use-images nil))
+
+(require 'magit-blame)
+(require 'quick-jump)
+
+
+;; (require 'hs-lint)
+
+;; (setq flymake-gui-warnings-enabled nil)
+
+
+
+
+
+;;
+;; Company Mode
+;;
+
+(require 'helm-config)
+
+(helm-mode 1)
+
+(global-set-key (kbd "C-x C-b")   'helm-buffers-list)
+(global-set-key (kbd "M-/")       'helm-dabbrev)
+
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; (delete 'company-semantic company-backends)
+(add-to-list 'company-backends 'company-c-headers)
+(add-to-list 'company-backends 'company-gtags)
+(add-to-list 'company-backends 'company-files)
+(add-to-list 'company-backends 'company-etags)
+(add-to-list 'company-backends 'company-clang)
+(add-to-list 'company-backends 'company-dabbrev)
+(add-to-list 'company-backends 'company-dabbrev-code)
+
+(add-hook 'c-mode-common-hook 'hs-minor-mode)
+
+;; (define-key c-mode-map  [(tab)] 'company-complete)
+;; (global-set-key [tab] 'company-complete)
+
+(require 'projectile)
+(projectile-global-mode)
+
+(require 'helm-projectile)
+(helm-projectile-on)
+
+
+(defadvice align-regexp (around align-regexp-with-spaces activate)
+  (let ((indent-tabs-mode nil))
+    ad-do-it))
+
