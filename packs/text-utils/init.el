c7b7661020751c478dab01053afc9a220e1e8e62
Alex Petrov <oleksandr.petrov@gmail.com>
2015-05-01 17:56:26 +0200

    Add text-utils and undo-tree

---
+++ packs/text-utils/init.el
@@ -0,0 +1,19 @@
+;; User pack init file
+;;
+;; User this file to initiate the pack configuration.
+;; See README for more information.
+
+
+;; (load "textmate")
+
+;; TODO maybe remove ido-hacks? Helm looks bit better
+;; (require 'ido-hacks)
+;; (ido-hacks-mode)
+
+;; (require 'textmate)
+;; (textmate-mode)
+
+(live-load-config-file "deft.el")
+
+;; Load bindings config
+(live-load-config-file "bindings.el")
