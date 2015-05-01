f8e459d9c4a27d19d00c1538f9d95a52965e57fd
Alex Petrov <oleksandr.petrov@gmail.com>
2015-05-01 17:55:31 +0200

    Add c-pack

---
+++ packs/c-pack/init.el
@@ -0,0 +1,15 @@
+;; User pack init file
+;;
+;; User this file to initiate the pack configuration.
+;; See README for more information.
+
+;; Load bindings config
+(live-load-config-file "bindings.el")
+
+(live-add-pack-lib "/xcscope/")
+
+(require 'xcscope)
+(cscope-setup)
+
+(setq c-default-style "gnu"
+      c-basic-offset 2)
