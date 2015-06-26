4ec19bb60f804ba29868a4a63a5165e95b3b0f00
Alex Petrov <oleksandr.petrov@gmail.com>
2015-06-26 11:06:42 +0200

    Fix coloring

--- etc/custom/custom-configuration.el
+++ etc/custom/custom-configuration.el
@@ -3,8 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(agda2-include-dirs (quote ("/Users/ifesdjeen/foss/haskell/agda-stdlib/src" ".")))
- '(company-backends (quote (company-abbrev company-dabbrev-code company-etags company-gtags company-c-headers company-elisp company-bbdb company-nxml company-css company-eclim company-clang company-xcode company-ropemacs company-cmake company-capf (company-dabbrev-code company-gtags company-etags company-keywords) company-oddmuse company-files company-dabbrev)))
+ '(agda2-include-dirs
+   (quote
+    ("/Users/ifesdjeen/foss/haskell/agda-stdlib/src" ".")))
+ '(company-backends
+   (quote
+    (company-abbrev company-dabbrev-code company-etags company-gtags company-c-headers company-elisp company-bbdb company-nxml company-css company-eclim company-clang company-xcode company-ropemacs company-cmake company-capf
+                    (company-dabbrev-code company-gtags company-etags company-keywords)
+                    company-oddmuse company-files company-dabbrev)))
  '(haskell-notify-p t)
  '(haskell-process-path-cabal "/usr/local/bin/cabal")
  '(haskell-process-type (quote cabal-repl))
@@ -30,6 +36,6 @@
  '(ediff-odd-diff-A ((((class color) (background dark)) (:background "dark green"))))
  '(ediff-odd-diff-B ((((class color) (background dark)) (:background "dark red"))))
  '(flymake-errline ((((class color)) (:underline "red"))))
- '(flymake-warnline ((((class color)) (:underline "yellow"))))
+ '(flymake-warnline ((t (:background "color-18" :underline "yellow"))))
  '(mumamo-background-chunk-major ((((class color) (background dark)) (:background "black"))))
  '(mumamo-background-chunk-submode1 ((((class color) (background dark)) (:background "black")))))
